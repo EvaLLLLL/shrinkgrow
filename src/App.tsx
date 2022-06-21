5bc98829230e8f6cf47fed6045e6569dda707a05
lyhhh <17610603221@163.com>
2022-06-21 19:49:15 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ShrinkButton />
     </div>
-  );
+  )
 }
 
-export default App;
+const MIN = 100,
+  MAX = 300,
+  STEP = 50,
+  TIME = 500
+
+const ShrinkButton: React.FC = () => {
+  const [status, setStatus] = React.useState<'shrink' | 'grow'>('shrink')
+  const [n, setN] = React.useState(MIN)
+
+  const timerRef = React.useRef<NodeJS.Timer | undefined>()
+
+  const shrinkOrGrow = () => {
+    timerRef.current = setInterval(() => {
+      setN(n => (status === 'shrink' ? n + STEP : n - STEP))
+      setStatus(status === 'shrink' ? 'grow' : 'shrink')
+    }, TIME)
+  }
+
+  React.useEffect(() => {
+    if (n >= MAX || n <= MIN) {
+      clearInterval(timerRef.current)
+    }
+  }, [n])
+
+  return (
+    <button onClick={shrinkOrGrow} style={{ width: n, height: n }}>
+      shrinkOrGrow
+    </button>
+  )
+}
+
+export default App
